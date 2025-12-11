b302cf93a1450f533861a30f856e21509155af7a
EDS3L <mBarlik84@gmail.com>
2025-12-11 18:13:59 +0100

    fix org size

--- core/src/main/java/pl/photodrive/core/application/command/album/DownloadFilesCommand.java
+++ core/src/main/java/pl/photodrive/core/application/command/album/DownloadFilesCommand.java
@@ -2,6 +2,6 @@ package pl.photodrive.core.application.command.album;
 
 import java.util.List;
 import java.util.UUID;
-//!
+
 public record DownloadFilesCommand(List<String> fileNames, UUID albumId) {
 }
